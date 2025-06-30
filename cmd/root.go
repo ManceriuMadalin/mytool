25fb0adf6f05287804492a9185df49b7786d7fb5
Madalin Manceriu <58183823+ManceriuMadalin@users.noreply.github.com>
2025-06-30 17:10:32 +0300

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,14 @@
+package cmd
 
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mytool",
+	Short: "MyTool is a CLI utility with multiple functions",
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
