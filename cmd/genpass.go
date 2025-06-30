9670bb7971880f27e698f360e2267efc30bd3105
Madalin Manceriu <58183823+ManceriuMadalin@users.noreply.github.com>
2025-06-30 17:10:16 +0300

    Update genpass.go

--- cmd/genpass.go
+++ cmd/genpass.go
@@ -1,1 +1,30 @@
+package cmd
 
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+
+	"github.com/spf13/cobra"
+)
+
+var length int
+
+var genpassCmd = &cobra.Command{
+	Use:   "genpass",
+	Short: "Generate a secure password",
+	Run: func(cmd *cobra.Command, args []string) {
+		chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+		result := ""
+		for i := 0; i < length; i++ {
+			num, _ := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+			result += string(chars[num.Int64()])
+		}
+		fmt.Println("Generated password:", result)
+	},
+}
+
+func init() {
+	genpassCmd.Flags().IntVarP(&length, "length", "l", 12, "Length of the password")
+	rootCmd.AddCommand(genpassCmd)
+}
